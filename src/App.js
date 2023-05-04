22667891dab5d3cce615e5bf62246dc9e990aad9
Ochuko George <georgeochuko7@gmail.com>
2023-05-04 12:29:35 +0100

    completed Nav-bar and Hero Banner sections

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
 
 import Home from './pages/Home'
 import ExerciseDetail from './pages/ExerciseDetail'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 const App = () => {
-  return (
-    <>
+   return (
+    <Box width='400px' sx={{width: {xl: '1488px'}}} m='auto'>
         <NavBar/>
         <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/exerciseDetail' element={<ExerciseDetail/>}/>
+            <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
         </Routes>
         <Footer/>
-    </>
+    </Box>
   )
 }
 
